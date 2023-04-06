18a62246702a541f332c610e916e481e2377d2af
ostapvasulenko <ostapvasilenko55@gmail.com>
2023-04-06 13:56:41 +0300

    Making regisration form and modal window in section Discover

--- src/js/script.js
+++ src/js/script.js
@@ -59,3 +59,50 @@ ButtonImage.addEventListener("click", function () {
   var element = document.getElementById("menu");
   element.scrollIntoView({ behavior: "smooth", block: "start" });
 });
+
+
+/* Registartion form modal */
+
+const registrationBtn = document.querySelector('.header__btn');
+const registrationFormWrapper = document.querySelector('.header__form-wrapper');
+
+registrationBtn.addEventListener('click', () => {
+  registrationFormWrapper.style.display = 'block';
+});
+
+registrationFormWrapper.addEventListener('click', (event) => {
+  if (event.target === registrationFormWrapper) {
+    registrationFormWrapper.style.display = 'none';
+  }
+});
+
+
+
+var myButton = document.getElementById("discover__btn");
+var myPopup = document.getElementById("discover__popup");
+var closeButton = document.getElementById("discover__close_button");
+
+// показуємо вікно з інформацією при натисканні на кнопку
+myButton.addEventListener("click", function() {
+  myPopup.style.display = "block";
+});
+
+// закриваємо вікно при натисканні на кнопку "Закрити вікно"
+closeButton.addEventListener("click", function() {
+  myPopup.style.display = "none";
+});
+
+myButton.addEventListener("click", function() {
+  myPopup.classList.add("show");
+});
+
+// закриваємо вікно при натисканні на кнопку "Закрити вікно"
+closeButton.addEventListener("click", function() {
+  myPopup.classList.remove("show");
+  myPopup.classList.add("hide");
+  
+  // зачекайте 0,3 секунди, поки анімація закінчиться, тоді приховайте вікно
+  setTimeout(function() {
+    myPopup.classList.remove("hide");
+  }, 500);
+});
